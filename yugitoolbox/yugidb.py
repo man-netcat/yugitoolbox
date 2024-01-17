1ad5e3d5f371b14bc60c315c43782d38e6ee898d
Rick Teuthof <rmt22198@outlook.com>
2024-01-17 16:47:53 +0100

    Filter functions refactored

--- yugitoolbox/yugidb.py
+++ yugitoolbox/yugidb.py
@@ -66,115 +66,108 @@ class YugiDB:
         return self._make_cards_list(self.card_query.all())
 
     def get_cards_by_values(self, params: dict) -> list[Card]:
+        def build_filter(key, column, values, condition=None, type_modifier=None):
+            if key not in params:
+                return None
+
+            def apply_modifier(value):
+                return column(type_modifier(value)) if type_modifier else column(value)
+
+            if "," in values:
+                values_list = values.split(",")
+                query = and_(*map(apply_modifier, values_list))
+            elif "|" in values:
+                values_list = values.split("|")
+                query = or_(*map(apply_modifier, values_list))
+            else:
+                query = apply_modifier(values)
+
+            return and_(condition, query) if condition else query
+
         filters = [
-            filter
-            for key, filter in [
-                ("name", Texts.name == params.get("name")),
-                ("id", Datas.id == int(params.get("id", 0))),
-                (
-                    "race",
-                    Datas.race == Race[params.get("race", "_")].value,
-                ),
-                (
-                    "attribute",
-                    Datas.attribute == Attribute[params.get("attribute", "_")].value,
-                ),
-                ("atk", Datas.atk == int(params.get("atk", 0))),
-                ("def", Datas.def_ == int(params.get("def", 0))),
-                (
-                    "level",
-                    Datas.level.op("&")(0x0000FFFF) == int(params.get("level", 0)),
-                ),
-                (
-                    "scale",
-                    and_(
-                        Datas.type.op("&")(Type.Pendulum.value),
-                        Datas.level.op(">>")(24) == int(params.get("scale", 0)),
-                    ),
-                ),
-                ("koid", Koids.koid == int(params.get("koid", 0))),
-                (
-                    "alltype",
-                    and_(
-                        *[
-                            Datas.type.op("&")(Type[t].value)
-                            for t in params.get("alltype", "_").split(",")
-                        ]
-                    ),
-                ),
-                (
-                    "anytype",
-                    or_(
-                        *[
-                            Datas.type.op("&")(Type[t].value)
-                            for t in params.get("anytype", "_").split(",")
-                        ]
-                    ),
-                ),
-                (
-                    "allcategory",
-                    and_(
-                        *[
-                            Datas.category.op("&")(Category[c].value)
-                            for c in params.get("allcategory", "_").split(",")
-                        ]
-                    ),
-                ),
-                (
-                    "anycategory",
-                    or_(
-                        *[
-                            Datas.category.op("&")(Category[c].value)
-                            for c in params.get("anycategory", "_").split(",")
-                        ]
-                    ),
-                ),
-                (
-                    "allgenre",
-                    and_(
-                        *[
-                            Datas.genre.op("&")(Genre[g].value)
-                            for g in params.get("allgenre", "_").split(",")
-                        ]
-                    ),
-                ),
-                (
-                    "anygenre",
-                    or_(
-                        *[
-                            Datas.genre.op("&")(Genre[g].value)
-                            for g in params.get("anygenre", "_").split(",")
-                        ]
-                    ),
-                ),
-                (
-                    "alllinkmarker",
-                    and_(
-                        Datas.type.op("&")(Type.Link.value),
-                        and_(
-                            *[
-                                Datas.def_.op("&")(LinkMarker[l].value)
-                                for l in params.get("alllinkmarker", "_").split(",")
-                            ]
-                        ),
-                    ),
-                ),
-                (
-                    "anylinkmarker",
-                    and_(
-                        Datas.type.op("&")(Type.Link.value),
-                        or_(
-                            *[
-                                Datas.def_.op("&")(LinkMarker[l].value)
-                                for l in params.get("anylinkmarker", "_").split(",")
-                            ]
-                        ),
-                    ),
-                ),
-            ]
-            if key in params
+            build_filter(
+                "name",
+                Texts.name.op("=="),
+                params.get("name"),
+            ),
+            build_filter(
+                "id",
+                Datas.id.op("=="),
+                params.get("id", 0),
+                type_modifier=int,
+            ),
+            build_filter(
+                "race",
+                Datas.race.op("=="),
+                params.get("race", "_"),
+                type_modifier=lambda x: Race[x].value,
+            ),
+            build_filter(
+                "attribute",
+                Datas.attribute,
+                params.get("attribute", "_"),
+                type_modifier=lambda x: Attribute[x].value,
+            ),
+            build_filter(
+                "atk",
+                Datas.atk,
+                params.get("atk", 0),
+                type_modifier=int,
+            ),
+            build_filter(
+                "def",
+                Datas.def_,
+                params.get("def", 0),
+                type_modifier=int,
+            ),
+            build_filter(
+                "level",
+                Datas.level.op("&")(0x0000FFFF).op("=="),
+                params.get("level", 0),
+                type_modifier=int,
+            ),
+            build_filter(
+                "scale",
+                Datas.level.op(">>")(24).op("=="),
+                params.get("scale", 0),
+                condition=Datas.type.op("&")(Type.Pendulum.value),
+                type_modifier=int,
+            ),
+            build_filter(
+                "koid",
+                Koids.koid.op("=="),
+                params.get("koid", 0),
+                type_modifier=int,
+            ),
+            build_filter(
+                "type",
+                Datas.type.op("&"),
+                params.get("type", "_"),
+                type_modifier=lambda x: Type[x].value,
+            ),
+            build_filter(
+                "category",
+                Datas.category.op("&"),
+                params.get("category", "_"),
+                type_modifier=lambda x: Category[x].value,
+            ),
+            build_filter(
+                "genre",
+                Datas.genre.op("&"),
+                params.get("genre", "_"),
+                type_modifier=lambda x: Genre[x].value,
+            ),
+            build_filter(
+                "linkmarker",
+                Datas.def_.op("&"),
+                params.get("linkmarker", "_"),
+                condition=Datas.type.op("&")(Type.Link.value),
+                type_modifier=lambda x: LinkMarker[x].value,
+            ),
         ]
-        return self._filter_query_builder(self.card_query, filters)
+        return self._filter_query_builder(
+            self.card_query, [filter for filter in filters if filter is not None]
+        )
 
     def get_cards_by_query(self, query: Callable[[Card], bool]) -> list[Card]:
         return [card for card in self.cards if query(card)]
