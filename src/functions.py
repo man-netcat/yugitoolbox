c8cf30eb9490ddcc53d5d60388c990eea8d28c50
Rick Teuthof <rmt22198@outlook.com>
2024-02-24 11:41:00 +0100

    Added some functions

--- src/functions.py
+++ src/functions.py
@@ -1,10 +1,12 @@
-from .omegadb import OmegaDB
-from itertools import product
+from itertools import combinations, product
+
+from .card import Card
 from .enums import *
+from .omegadb import YugiDB
 
 
 def rescue_hedgehog(
-    db,
+    db: YugiDB,
     level: str = None,
     include_ocg_only=False,
     include_tcg_only=False,
@@ -45,3 +47,48 @@ def rescue_hedgehog(
         )
     ]
     return pairs
+
+
+def numbers_eveil(db: YugiDB, fifth: int):
+    def extract_integer(string):
+        import re
+
+        match = re.search(r"Number\s+i?[CSF]?(\d+)", string)
+        if match:
+            return int(match.group(1))
+        return None
+
+    number_monsters = db.get_cards_by_values(
+        {
+            "in_name": "Number,~Number XX,~Number ic1000,~Number C1000",
+            "type": "xyz",
+        }
+    )
+
+    mapping = {card: extract_integer(card.name) for card in number_monsters}
+
+    combos = [
+        combo
+        for combo in combinations(number_monsters, 4)
+        if (
+            sum([mapping[card] for card in combo]) == fifth
+            and len(set([card.level for card in combo])) == 4
+        )
+    ]
+
+    return combos
+
+
+def duality(db: YugiDB, card: Card):
+    if not card.attribute in [Attribute.DARK, Attribute.LIGHT]:
+        return []
+
+    targets = db.get_cards_by_values(
+        {
+            "race": card.race,
+            "level": card.level,
+            "attribute": Attribute(0x30 - card.attribute),
+        }
+    )
+
+    return targets
